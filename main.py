e2c07fba722a0cbecc584612fd249cd5033371f3
leo <luisleo526@yahoo.com>
2022-12-29 23:46:41 +0800

    add test

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import logging
-import math
 from argparse import ArgumentParser
+from datetime import datetime
 
+import math
+import torch
 import yaml
 from accelerate import Accelerator
 from accelerate.logging import get_logger
+from monai.inferers import sliding_window_inference
 from monai.metrics import DiceMetric
 from monai.transforms import AsDiscrete
 from munch import DefaultMunch
+from torch.utils.tensorboard import SummaryWriter
 from tqdm.auto import tqdm
 
 from dataloaders import get_dataloaders
-from utils import get_class, get_MSD_dataset_properties
 from model_wrapper import SegmentationModel
+from utils import get_class, get_MSD_dataset_properties
 
 logger = get_logger(__name__)
 
@@ -25,9 +29,7 @@ def parse_args():
 
 
 def main(args):
-    accelerator = Accelerator(gradient_accumulation_steps=args.TRAIN.gradient_accumulation_steps,
-                              log_with="tensorboard", logging_dir="./log")
-    accelerator.init_trackers(project_name=args.GENERAL.task)
+    accelerator = Accelerator(gradient_accumulation_steps=args.TRAIN.gradient_accumulation_steps)
     device = accelerator.device
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
@@ -40,6 +42,10 @@ def main(args):
     model = SegmentationModel(args).to(device)
     optimizer = get_class(args.TRAIN.optimizer.type)(model.parameters(), **args.TRAIN.optimizer.params)
     scheduler = get_class(args.TRAIN.scheduler.type)(optimizer, **args.TRAIN.scheduler.params)
+    if accelerator.is_main_process:
+        writer = SummaryWriter(f"./logs/{args.GENERAL.task}/{datetime.now().strftime('%Y%m%d-%H%M%S')}")
+    else:
+        writer = None
 
     # for metrics
     properties = get_MSD_dataset_properties(args)
@@ -58,8 +64,9 @@ def main(args):
 
     step = 0
     for epoch in range(args.TRAIN.max_epochs):
+        step = step + 1
 
-        total_loss = 0
+        results = dict(total_loss=dict(train=0, test=0))
         model.train()
         for batch in train_loader:
             with accelerator.accumulate(model):
@@ -69,24 +76,40 @@ def main(args):
                 scheduler.step()
                 optimizer.zero_grad()
 
-            total_loss += accelerator.gather(loss.detach().float())
+            results['total_loss']['train'] += accelerator.gather(loss.detach().float()).item()
             pred = accelerator.gather(pred.contiguous())
             target = accelerator.gather(batch["label"].contiguous())
             pred = [post_pred(i) for i in pred]
             target = [post_label(i) for i in target]
             metrics(pred, target)
 
-            if accelerator.sync_gradients:
-                progress_bar.update(1)
-
-        scores = [x * 100 for x in list(metrics.aggregate(reduction='mean_batch').cpu().numpy())]
-        info = {'loss': total_loss.item()}
-        for i in range(1, len(labels)):
-            info[labels[str(i)]] = scores[i - 1]
-        accelerator.log(info, step=step)
-        step = step + 1
-        logger.info(f"MeanDice Score: {scores}, Loss: {total_loss.item()}")
+        for i, score in list(metrics.aggregate(reduction='mean_batch').cpu().numpy()):
+            results[labels[str(i + 1)]] = dict(train=score, test=0)
         metrics.reset()
+
+        model.eval()
+        for batch in val_loader:
+            with torch.no_grad:
+                loss, pred = sliding_window_inference(inputs=batch, roi_size=args.TRANSFORM.patch_size,
+                                                      sw_batch_size=args.TRAIN.batch_size * args.TRANSFORM.num_samples,
+                                                      predictor=model)
+
+                results['total_loss']['test'] += accelerator.gather(loss.detach().float()).item()
+                pred = accelerator.gather(pred.contiguous())
+                target = accelerator.gather(batch["label"].contiguous())
+                pred = [post_pred(i) for i in pred]
+                target = [post_label(i) for i in target]
+                metrics(pred, target)
+
+        for i, score in list(metrics.aggregate(reduction='mean_batch').cpu().numpy()):
+            results[labels[str(i + 1)]]['test'] = score
+
+        if accelerator.is_main_process:
+            for key, value in results.items():
+                writer.add_scalars(key, value, global_step=step)
+
+        if accelerator.sync_gradients:
+            progress_bar.update(1)
 
 
 if __name__ == '__main__':
