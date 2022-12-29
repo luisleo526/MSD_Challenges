038ab625992bf3c1377357d74a4b987b24b34592
leo <luisleo526@yahoo.com>
2022-12-30 00:58:32 +0800

    debug

--- model_wrapper.py
+++ model_wrapper.py
@@ -15,7 +15,7 @@ class SegmentationModel(nn.Module):
         if type(input) is dict:
             return self.loss_fn(self.model(input["image"]), input["label"])
         elif torch.is_tensor(input) and label is None:
-            return torch.unbind(self.model(input), dim=1)[0]
+            return self.model(input)
         elif torch.is_tensor(input) and torch.is_tensor(label):
             return DiceCELoss(include_background=False, softmax=True, reduction='sum', to_onehot_y=True)(input, label)
         else:
